aa62ce02f2dedf6518155456526bf3f26962c424
theEthicalDev <djordje.karadzic@cybethics.com>
2026-01-24 12:56:12 +0100

    Update event helper and form control components: * Modify getTarget method to accept undefined as a valid target type. * Adjust label class in form-control-input-field-content-switch for improved styling. * Update fromKeenIcon method to handle null subscription and adjust cssClass accordingly.

--- event/event-helper.ts
+++ event/event-helper.ts
@@ -1,5 +1,5 @@
 export class EventHelper {
-  static getTarget<T>(target: EventTarget | null, type: new () => T): T {
+  static getTarget<T>(target: EventTarget | null | undefined, type: new () => T): T {
     if (target instanceof HTMLElement) {
       return target as unknown as T;
     }
--- form-control/input-field/form-control-input-field-content-param-icon.ts
+++ form-control/input-field/form-control-input-field-content-param-icon.ts
@@ -8,7 +8,7 @@ export class FormControlInputFieldContentParamIcon {
   public model: string;
   private subscription: Observable<any> | null = null;
 
-  public static fromKeenIcon(subscription: Observable<any>,
+  public static fromKeenIcon(subscription: Observable<any> | null,
                              icon: string,
                              cssClass: string | null = null,
                              color: string | null = null,
@@ -18,7 +18,7 @@ export class FormControlInputFieldContentParamIcon {
     const keenIcon = new FormControlInputFieldContentParamIcon();
     keenIcon.type = 'keenIcon';
     keenIcon.icon = icon;
-    keenIcon.cssClass = cssClass ?? 'fs-2 clickable';
+    keenIcon.cssClass = cssClass ?? 'fs-2 ' + (subscription ? ' clickable ' : '');
     keenIcon.color = color ?? 'text-gray-900';
     keenIcon.model = model ?? 'duotone';
     keenIcon.subscription = subscription;
